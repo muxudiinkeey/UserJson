e52116df3cc53317569ca0c36c9a076ef2b5b0a5
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-07 18:05:45 +0100

    fetch data httpClient

--- users/src/app/app.component.ts
+++ users/src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 // import { UsersComponent } from './users/users.component';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users/users.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [ CommonModule, UsersComponent],
+  imports: [ CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'users';
+  data: any;
+ private    http = inject(HttpClient);
+//  private Url = 'https://jsonplaceholder.typicode.com/users';
+
+fetchData() {
+this.http.get( 'https://jsonplaceholder.typicode.com/users')
+.subscribe((response)=>{this.data = response});
+}
 }
