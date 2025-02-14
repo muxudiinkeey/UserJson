ce8e0822c5e1b9378160dc36fe1bc8fae982e383
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-14 19:22:13 +0100

    userRegister

--- users/src/app/app.component.ts
+++ users/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users/users.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ CommonModule, UsersComponent],
+  imports: [ CommonModule, UsersComponent, UserRegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
