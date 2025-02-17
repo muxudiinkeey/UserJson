eeeb942e6c510944dce3a6d5ef9e088d29d9853e
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-17 13:54:05 +0100

    ok

--- users/src/app/users/users.service.ts
+++ users/src/app/users/users.service.ts
@@ -6,10 +6,13 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class UsersService {
-
+  //http
   http = inject (HttpClient);
+  // url
   private Url = 'https://jsonplaceholder.typicode.com/users';
+  //interface
   users: Iusers[] = [];
+  // get all users
 
  getUsers(){
   return this.http.get<Iusers[]>(this.Url);
